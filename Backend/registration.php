2b8e3a623295d7dbf7e61eb6ca4c888aa8de9455
neelsarvaiya <neelsarvaiya11@gmail.com>
2025-02-05 20:53:03 +0530

    user exist check

--- Backend/registration.php
+++ Backend/registration.php
@@ -4,64 +4,74 @@ include_once('connection.php');
 
 if (isset($_POST['register'])) {
 
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
-  $pic = $_FILES['pic']['name'];
-  $address = $_POST['address'];
-  $pin = $_POST['pin'];
-  $dob = $_POST['dob'];
-  $password = $_POST['password2'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $pic = $_FILES['pic']['name'];
+    $address = $_POST['address'];
+    $pin = $_POST['pin'];
+    $dob = $_POST['dob'];
+    $password = $_POST['password2'];
+
+    $user_exist = "SELECT Email FROM `register` WHERE Email = '$email'";
+    $result = mysqli_query($conn, $user_exist);
 
 
-  $data = "INSERT INTO `register`(`Full_Name`, `Email`, `Phone_number`, `Profile_pic`, `Address`, `Pincode_number`, `DOB`, `Password`) VALUES ('$name','$email','$phone','$pic','$address','$pin','$dob','$password')";
+    if (mysqli_num_rows($result) > 0) {
+        echo "<script>
+                 alert('$email-already Exist');
+                 window.location.href = '../index.php';
+              </script>";
+              exit();
+    } else {
+        $data = "INSERT INTO `register`(`Full_Name`, `Email`, `Phone_number`, `Profile_pic`, `Address`, `Pincode_number`, `DOB`, `Password`) VALUES ('$name','$email','$phone','$pic','$address','$pin','$dob','$password')";
 
-   mysqli_query($conn, $data);
-
+        mysqli_query($conn, $data);
+    }
 }
 ?>
 
 <?php
-    if (isset($_POST['register'])) {
+if (isset($_POST['register'])) {
 
-        $ftype = $_FILES['pic']['type'];
-        $fsize = $_FILES['pic']['size'];
-        if ($ftype == "image/png" || $ftype == "image/jpg") {
+    $ftype = $_FILES['pic']['type'];
+    $fsize = $_FILES['pic']['size'];
+    if ($ftype == "image/png" || $ftype == "image/jpg") {
 
-            if ($fsize <= 1024 * 1024) {
+        if ($fsize <= 1024 * 1024) {
 
-                if (!is_dir("uploads")) {
-                    mkdir("uploads");
-                }
+            if (!is_dir("uploads")) {
+                mkdir("uploads");
+            }
 
-                $fname = uniqid() . $_FILES['pic']['name'];
-                if (move_uploaded_file($_FILES['pic']['tmp_name'], "uploads/" . $fname)) {
-                    echo "<script>
+            $fname = uniqid() . $_FILES['pic']['name'];
+            if (move_uploaded_file($_FILES['pic']['tmp_name'], "uploads/" . $fname)) {
+                echo "<script>
                             alert('Registration SuccessFully...');
                             window.location.href = '../index.php';
                       </script>";
-                }else{
-                  ?>
-                  <script>
-                    alert('Registration not successfull..');
-                    window.location.href = '../index.php';
-                    </script>
-                  <?php
-                }
             } else {
-    ?>
+?>
                 <script>
-                    alert('File is larger than 1 KB. Please select a smaller file.')
+                    alert('Registration Failed..');
+                    window.location.href = '../index.php';
                 </script>
-
             <?php
             }
         } else {
             ?>
             <script>
-                alert('File is not a png or jpg file')
+                alert('File is larger than 1 KB. Please select a smaller file.')
             </script>
-    <?php
+
+        <?php
         }
+    } else {
+        ?>
+        <script>
+            alert('File is not a png or jpg file')
+        </script>
+<?php
     }
-    ?>
+}
+?>
