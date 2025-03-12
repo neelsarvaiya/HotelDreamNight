c958bfeec692951caca3540e8d4f15b491ec6471
kiritkanjariya69 <kiritkanjariya69@gmail.com>
2025-03-12 13:16:55 +0530

    About us page is daynamic

--- Admin/users.php
+++ Admin/users.php
@@ -5,6 +5,7 @@ include_once('inc/admin-header.php');
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
+
 <!-- DataTables CSS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
